f2e0675f6243e4f70b0cb5fe6cac828054c0f33c
Karen Painter <klpainter@gmail.com>
2018-12-10 09:50:28 -0800

    Seed data org and booking (#1)
    * restored server tests
    
    * initial Model
    
    * initial seed data writing to database
    
    * added disconnect afterAll
    
    * added fix (I hope) to travis error
    
    * added mongo
    
    * added more Booking fields

---
+++ server/tests/testData.js
@@ -0,0 +1,25 @@
+export const org1 = { name: 'ACLU Oregon' };
+export const org1Booking1 = {
+  swisId: '12345',
+  preferredName: 'Boomer Smith',
+  gender: 'Non-binary',
+  pronouns: 'he/him',
+  primaryOrgContact: 'Molly Helper',
+  contacts: 'Mother:  503-123-1234',
+  upcomingDates: 'Arraignment on 12/30/2018',
+  notes: 'Not even part of protest.',
+  mcsoName: 'Smith, Henry James',
+  mcsoAge: 32,
+  mcsoGender: 'M',
+  mcsoRace: 'White',
+  mcsoHeight: '5 ft 10 in',
+  mcsoWeight: '160 lbs',
+  mcsoHair: 'Brown',
+  mcsoEyes: 'Blue',
+  mcsoArrestingAgency: 'Portland Police, Central Precinct',
+  mcsoBookingDate: '10/20/2018 6:33 PM',
+  mcsoAssignedFacility: 'MCDC',
+  mcsoProjectedReleaseDate: 'Unknown'
+};
+
+export const org2 = { name: 'Legal Observers'};
