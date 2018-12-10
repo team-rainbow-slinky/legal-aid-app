aa39d6e5a5bb436b01009fcb8a2e821ce373fc31
KLPainter <KLPainter@gmail.com>
2018-12-10 11:19:45 -0800

    added users and tokens

--- server/tests/testHelper.js
+++ server/tests/testHelper.js
@@ -3,12 +3,14 @@ config();
 
 import mongoose from 'mongoose';
 import { connect } from '../src/utils/connect';
-// import request from 'supertest';
+import request from 'supertest';
+import app from '../src/routes/app';
 import Org from '../src/models/Org';
 import Booking from '../src/models/Booking';
+import User from '../src/models/User';
 import {
-  org1, org1Booking1,
-  org2
+  org1, org1User1, org1User2, org1Booking1, org1Booking2,
+  org2, org2User1, org2User2, org2Booking1, org2Booking2,
 } from './testData';
 
 connect ('mongodb://localhost:27017/legal-aid-test');
@@ -32,7 +34,75 @@ beforeEach(() => {
   return Org.findOne(org1)
     .then(org => {
       return Booking.create(
-        { ...org1Booking1, org: org}
+        { ...org1Booking1, org: org},
+        { ...org1Booking2, org: org}
+      )
+    });
+});
+
+beforeEach(() => {
+  return Org.findOne(org1)
+    .then(org => {
+      return User.create(
+        { ...org1User1, org: org},
+        { ...org1User2, org: org}
+      )
+    });
+});
+
+beforeEach(() => {
+  return Org.findOne(org2)
+    .then(org => {
+      return Booking.create(
+        { ...org2Booking1, org: org},
+        { ...org2Booking2, org: org}
+      )
+    });
+});
+
+let org1User1Token;
+beforeEach(() => {
+  return request(app)
+    .post('/api/users/login')
+    .send(org1User1)
+    .then(res => res.get('X-AUTH-TOKEN'))
+    .then(headerToken => org1User1Token = headerToken);
+});
+
+let org1User2Token;
+beforeEach(() => {
+  return request(app)
+    .post('/api/users/login')
+    .send(org1User2)
+    .then(res => res.get('X-AUTH-TOKEN'))
+    .then(headerToken => org1User2Token = headerToken);
+});
+
+let org2User1Token;
+beforeEach(() => {
+  return request(app)
+    .post('/api/users/login')
+    .send(org2User1)
+    .then(res => res.get('X-AUTH-TOKEN'))
+    .then(headerToken => org1User1Token = headerToken);
+});
+
+let org2User2Token;
+beforeEach(() => {
+  return request(app)
+    .post('/api/users/login')
+    .send(org2User2)
+    .then(res => res.get('X-AUTH-TOKEN'))
+    .then(headerToken => org1User2Token = headerToken);
+});
+
+
+beforeEach(() => {
+  return Org.findOne(org2)
+    .then(org => {
+      return User.create(
+        { ...org2User1, org: org},
+        { ...org2User2, org: org}
       )
     });
 });
@@ -49,3 +119,10 @@ export const setupTestGetters = Model => {
 
 export const [getOrg, getOrgs] = setupTestGetters(Org);
 export const [getBooking, getBookings] = setupTestGetters(Booking);
+export const [getUser, getUsers] = setupTestGetters(User);
+export const getTokens = () => {
+  org1User1: org1User1Token
+  // org1User2: org1User2Token,
+  // org2User1: org2User1Token,
+  // org2User2: org2User2Token
+};
