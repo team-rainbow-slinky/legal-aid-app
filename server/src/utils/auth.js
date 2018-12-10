3e3da199081de6f83d553c94576f724eefdefe08
KLPainter <KLPainter@gmail.com>
2018-12-10 10:25:53 -0800

    in support of auth

---
+++ server/src/utils/auth.js
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+import { sign, verify } from 'jsonwebtoken';
+import { compareSync, hashSync } from 'bcryptjs';
+
+config();
+
+const APP_SECRET = process.env.APP_SECRET;
+const EXPIRE_IN = '24h';
+
+
+export const hash = password => hashSync(password, 8);
+
+export const compare = (password, hash) => compareSync(password, hash);
+
+export const tokenize = payload => sign({ payload }, APP_SECRET, { expiresIn: EXPIRE_IN })
+
+export const untokenize = token => verify(token, APP_SECRET).payload;
