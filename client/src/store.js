7dbf5b07f15d2eff5f8600e6efe53d3803fbe8ff
Claire Flanagan <claireflanagan@gmail.com>
2018-12-11 10:31:21 -0800

    auth working!

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
 import { middleware } from './middleware/index';
 
