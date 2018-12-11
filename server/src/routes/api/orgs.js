8381982a9260b8a6f7291dd85d3786ab4fd1cce4
Karen Painter <klpainter@gmail.com>
2018-12-11 15:17:08 -0800

    Feature/get with scraper (#13)
    *  added node parser
    
    * added node parser
    
    * added mock data
    
    * mods in support of testing
    
    * basic filtered get of mcso data is working!
    
    * changed msco to mcso

--- server/src/routes/api/orgs.js
+++ server/src/routes/api/orgs.js
@@ -4,6 +4,7 @@ import Booking from '../../models/Booking';
 import { HttpError } from '../../middleware/error';
 import requireAuth from '../../middleware/requireAuth';
 import confirmOrg from '../../middleware/confirmOrg';
+import { getMcsoRecords } from '../../services/scraper';
 
 export default Router()
   .get('/:orgId', requireAuth, confirmOrg, (req, res, next) => {
@@ -21,4 +22,22 @@ export default Router()
     .lean()
     .then(bookings => res.json(bookings))
     .catch(next);
+  })
+
+  .get('/:orgId/mcso', (req, res, next) => {
+    const { orgId } = req.params;
+    const query = { org: orgId };
+
+    const promises = [
+      Booking.find(query)
+              .then(bookings => bookings.map(booking => booking.swisId)),
+      getMcsoRecords()
+    ];
+
+    Promise.all(promises)
+      .then(([swisIds, mcsoBookings]) => {
+        return mcsoBookings.filter(mcso => !swisIds.includes(mcso.swisId))
+      })
+      .then(filteredResults => res.json(filteredResults))
+      .catch(next);
   });
