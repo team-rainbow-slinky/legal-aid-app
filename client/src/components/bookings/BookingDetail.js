2b208ba7ffa8fe146eb3c410eb22f560f9f920d7
Alex Rankin <alex.c.rankin@gmail.com>
2018-12-12 12:44:57 -0800

    stateful form

--- client/src/components/bookings/BookingDetail.js
+++ client/src/components/bookings/BookingDetail.js
@@ -12,7 +12,6 @@ export default class BookingDetail extends PureComponent {
 
   state = {
     editing: false,
-    swisId: '',
     preferredName: '',
     gender: '',
     pronouns: '',
@@ -38,7 +37,6 @@ export default class BookingDetail extends PureComponent {
     if(prevProps !== this.props){
       const { booking } = this.props;
       this.setState({
-        swisId: booking.swisId,
         preferredName: booking.preferredName,
         gender: booking.gender,
         pronouns: booking.pronouns,
@@ -81,13 +79,25 @@ export default class BookingDetail extends PureComponent {
   };
 
   render() {
-    // const { editing } = this.state;
     const { booking } = this.props;
     if(!booking) return null;
+    const {
+      editing,
+      preferredName,
+      gender,
+      pronouns,
+      primaryOrgContact,
+      contacts,
+      upcomingDates,
+      notes
+    } = this.state;
+
     return (
       <>
         <h3>Booking</h3>
         <div>
+          <h3>MCSO Data</h3>
+          <p>SwisID: {booking.swisId}</p>
           <p>Legal Name: {booking.mcsoName}</p>
           <p>Age: {booking.mcsoAge}</p>
           <p>Gender: {booking.mcsoGender}</p>
@@ -100,8 +110,34 @@ export default class BookingDetail extends PureComponent {
           <p>Booking Date: {booking.mcsoBookingDate}</p>
           <p>Assigned Facility: {booking.mcsoAssignedFacility}</p>
           <p>Projected Release Date: {booking.mcsoProjectedReleaseDate}</p>
-
         </div>
+
+        <form>
+          <fieldset>
+            <legend>NLG Data</legend>
+            <label htmlFor="preferredName">Preferred Name: </label>
+            <input name="preferredName" onChange={this.handleChange} value={preferredName}></input>
+
+            <label htmlFor="gender">Gender: </label>
+            <input name="gender" onChange={this.handleChange} value={gender}></input>
+
+            <label htmlFor="pronouns">Pronouns: </label>
+            <input name="pronouns" onChange={this.handleChange} value={pronouns}></input>
+
+            <label htmlFor="primaryOrgContact">Primary Contact at NLG: </label>
+            <input name="primaryOrgContact" onChange={this.handleChange} value={primaryOrgContact}></input>
+
+            <label htmlFor="contacts">Contacts: </label>
+            <input name="contacts" onChange={this.handleChange} value={contacts}></input>
+
+            <label htmlFor="upcomingDates">Upcoming Dates: </label>
+            <input name="upcomingDates" onChange={this.handleChange} value={upcomingDates}></input>
+
+            <label htmlFor="notes">Notes: </label>
+            <input name="notes" onChange={this.handleChange} value={notes}></input>
+
+          </fieldset>
+        </form>
         {/* {editing ?
           <BookingEdit booking={booking}/> : */}
           {/* // <BookingDisplay booking={booking}/>} */}
