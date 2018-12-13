b42953fd944fc1bcdd56891aebe3cf72c78f5ec6
Alex Rankin <alex.c.rankin@gmail.com>
2018-12-13 11:58:08 -0800

    rendering charges

--- client/src/components/bookings/Booking.js
+++ client/src/components/bookings/Booking.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import { ROUTES } from '../../routes/routes';
 import styles from './Booking.css';
 
-export default function Booking({ _id, preferredName, mcsoBookingDate, mcsoArrestingAgency }) {
+export default function Booking({ _id, preferredName, mcsoName, mcsoBookingDate, mcsoArrestingAgency }) {
   return (
     <Link key={_id} to={ROUTES.BOOKING_DETAIL.linkTo(_id)} className={styles.bookingLink}>
       <div className={styles.orgBooking}>
-        <p>{preferredName}, booked on {mcsoBookingDate} by {mcsoArrestingAgency}</p>
+        <p>{preferredName || mcsoName}, booked on {mcsoBookingDate} by {mcsoArrestingAgency}</p>
       </div>
     </Link>
   );
