9245e015bee4e8c829e1bda5c670df0ad18b4461
KLPainter <KLPainter@gmail.com>
2018-12-13 10:46:28 -0800

    not working

---
+++ server/heroku/dataCreator.js
@@ -0,0 +1,32 @@
+import { config } from 'dotenv';
+config();
+
+import mongoose from 'mongoose';
+import { connect } from '../src/utils/connect';
+import Org from '../src/models/Org';
+import User from '../src/models/User';
+import {
+  org1, org1User1, org1User2,
+  org2, org2User1, org2User2
+} from './initialData';
+
+connect ();
+//return mongoose.connection.dropDatabase();
+generateData();
+return mongoose.disconnect();
+
+async function generateData() {
+  let orgs;
+  orgs = await Org.create(
+    org1,
+    org2
+  );
+  await User.create(
+    { ...org1User1, org: orgs[0] },
+    { ...org1User2, org: orgs[0] }
+  );
+  await User.create(
+    { ...org2User1, org: orgs[1] },
+    { ...org2User2, org: orgs[1] }
+  );
+}
