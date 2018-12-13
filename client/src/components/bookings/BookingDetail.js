a074075a4e200098f7c5cacafa62d7687ac032a4
Alex Rankin <alex.c.rankin@gmail.com>
2018-12-13 10:06:51 -0800

    canceling edit

--- client/src/components/bookings/BookingDetail.js
+++ client/src/components/bookings/BookingDetail.js
@@ -22,18 +22,23 @@ export default class BookingDetail extends PureComponent {
 
   componentDidUpdate(prevProps) {
     if(prevProps !== this.props){
-      const { booking } = this.props;
-      this.setState({
-        preferredName: booking.preferredName,
-        gender: booking.gender,
-        pronouns: booking.pronouns,
-        primaryOrgContact: booking.primaryOrgContact,
-        contacts: booking.contacts,
-        upcomingDates: booking.upcomingDates,
-        notes: booking.notes
-      });
+      this.revertState();
     }
   }
+
+  revertState = () => {
+    const { booking } = this.props;
+    this.setState({
+      preferredName: booking.preferredName,
+      gender: booking.gender,
+      pronouns: booking.pronouns,
+      primaryOrgContact: booking.primaryOrgContact,
+      contacts: booking.contacts,
+      upcomingDates: booking.upcomingDates,
+      notes: booking.notes
+    });
+  };
+
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
   };
@@ -44,12 +49,13 @@ export default class BookingDetail extends PureComponent {
 
   handleCancel = () => {
     this.setState({ editing: false });
+    this.revertState();
   };
 
   handleSubmit = event => {
     event.preventDefault();
     this.props.editBooking({ ...this.props.booking, ...this.state });
-    this.handleCancel();
+    this.setState({ editing: false });
   };
 
   render() {
@@ -69,58 +75,58 @@ export default class BookingDetail extends PureComponent {
     return (
       <>
         <Header />
-        <h3>Booking</h3>
-        <div>
-          <h3>MCSO Data</h3>
-          <p>SwisID: {booking.swisId}</p>
-          <p>Legal Name: {booking.mcsoName}</p>
-          <p>Age: {booking.mcsoAge}</p>
-          <p>Gender: {booking.mcsoGender}</p>
-          <p>Race: {booking.mcsoRace}</p>
-          <p>Height: {booking.mcsoHeight}</p>
-          <p>Weight: {booking.mcsoWeight}</p>
-          <p>Hair: {booking.mcsoHair}</p>
-          <p>Eyes: {booking.mcsoEyes}</p>
-          <p>Arresting Agency: {booking.mcsoArrestingAgency}</p>
-          <p>Booking Date: {booking.mcsoBookingDate}</p>
-          <p>Assigned Facility: {booking.mcsoAssignedFacility}</p>
-          <p>Projected Release Date: {booking.mcsoProjectedReleaseDate}</p>
+        <div className={styles.detailWrapper}>
+          <form className={styles.form} onSubmit={this.handleSubmit}>
+            <legend>NLG Data</legend>
+            <label htmlFor="preferredName">Preferred Name: </label>
+            <input name="preferredName" onChange={this.handleChange} value={preferredName} disabled={editing ? false : true}></input>
+
+            <label htmlFor="gender">Gender: </label>
+            <input name="gender" onChange={this.handleChange} value={gender} disabled={editing ? false : true}></input>
+
+            <label htmlFor="pronouns">Pronouns: </label>
+            <input name="pronouns" onChange={this.handleChange} value={pronouns} disabled={editing ? false : true}></input>
+
+            <label htmlFor="contacts">Personal Contacts: </label>
+            <input name="contacts" onChange={this.handleChange} value={contacts} disabled={editing ? false : true}></input>
+
+            <label htmlFor="primaryOrgContact">Primary Contact at NLG: </label>
+            <input name="primaryOrgContact" onChange={this.handleChange} value={primaryOrgContact} disabled={editing ? false : true}></input>
+
+            <label htmlFor="upcomingDates">Upcoming Dates: </label>
+            <input name="upcomingDates" onChange={this.handleChange} value={upcomingDates} disabled={editing ? false : true}></input>
+
+            <label htmlFor="notes">Notes: </label>
+            <input name="notes" onChange={this.handleChange} value={notes} disabled={editing ? false : true}></input>
+
+            {editing &&
+              <>
+                <button type="reset" onClick={this.handleCancel}>Cancel</button>
+                <button type="submit">Save Changes</button>
+              </>
+            }
+            {!editing &&
+              <button onClick={this.handleEdit}>Edit Booking</button>
+            }
+          </form>
+
+          <div>
+            <h3>MCSO Data</h3>
+            <p>SwisID: {booking.swisId}</p>
+            <p>Legal Name: {booking.mcsoName}</p>
+            <p>Age: {booking.mcsoAge}</p>
+            <p>Gender: {booking.mcsoGender}</p>
+            <p>Race: {booking.mcsoRace}</p>
+            <p>Height: {booking.mcsoHeight}</p>
+            <p>Weight: {booking.mcsoWeight}</p>
+            <p>Hair: {booking.mcsoHair}</p>
+            <p>Eyes: {booking.mcsoEyes}</p>
+            <p>Arresting Agency: {booking.mcsoArrestingAgency}</p>
+            <p>Booking Date: {booking.mcsoBookingDate}</p>
+            <p>Assigned Facility: {booking.mcsoAssignedFacility}</p>
+            <p>Projected Release Date: {booking.mcsoProjectedReleaseDate}</p>
+          </div>
         </div>
-
-        <form className={styles.form} onSubmit={this.handleSubmit}>
-
-          <legend>NLG Data</legend>
-          <label htmlFor="preferredName">Preferred Name: </label>
-          <input name="preferredName" onChange={this.handleChange} value={preferredName} disabled={editing ? false : true}></input>
-
-          <label htmlFor="gender">Gender: </label>
-          <input name="gender" onChange={this.handleChange} value={gender} disabled={editing ? false : true}></input>
-
-          <label htmlFor="pronouns">Pronouns: </label>
-          <input name="pronouns" onChange={this.handleChange} value={pronouns} disabled={editing ? false : true}></input>
-
-          <label htmlFor="contacts">Personal Contacts: </label>
-          <input name="contacts" onChange={this.handleChange} value={contacts} disabled={editing ? false : true}></input>
-
-          <label htmlFor="primaryOrgContact">Primary Contact at NLG: </label>
-          <input name="primaryOrgContact" onChange={this.handleChange} value={primaryOrgContact} disabled={editing ? false : true}></input>
-
-          <label htmlFor="upcomingDates">Upcoming Dates: </label>
-          <input name="upcomingDates" onChange={this.handleChange} value={upcomingDates} disabled={editing ? false : true}></input>
-
-          <label htmlFor="notes">Notes: </label>
-          <input name="notes" onChange={this.handleChange} value={notes} disabled={editing ? false : true}></input>
-
-          {editing &&
-            <>
-              <button type="reset" onClick={this.handleCancel}>Cancel</button>
-              <button type="submit">Save Changes</button>
-            </>
-          }
-          {!editing &&
-            <button onClick={this.handleEdit}>Edit Booking</button>
-          }
-        </form>
       </>
     );
   }
