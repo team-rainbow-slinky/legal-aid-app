fdde5fe33970e483ef27ee3af49a5f9d8658f66a
Karen Painter <klpainter@gmail.com>
2018-12-13 12:02:09 -0800

    Feature/add seed script (#22)
    * not working
    
    * this works!

---
+++ server/heroku/initialData.js
@@ -0,0 +1,6 @@
+export const org1 = { name: 'National Lawyers Guild' };
+export const org2 = { name: 'ACLU of Oregon' };
+export const org1User1 = { email: 'guild1@gmail.com', password: 'password' };
+export const org1User2 = { email: 'guild2@gmail.com', password: 'password' };
+export const org2User1 = { email: 'aclu1@gmail.com', password: 'password' };
+export const org2User2 = { email: 'aclu2@gmail.com', password: 'password' };
